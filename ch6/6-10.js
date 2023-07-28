adb5995de3c05f0b9f354b5c1f7b617bb43250b7
Tae-hyeon Kim <simoltobella@gmail.com>
2023-07-28 19:36:27 +0900

    Refactor: 6-10.js
    함수를 변환 함수로 묶음

--- ch6/6-10.js
+++ ch6/6-10.js
@@ -1,7 +1,24 @@
+import _ from 'lodash'
+
 const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 }
 
 export function acquireReading() {
   return reading
+}
+
+export function enrichReading(original) {
+  // const result = { ...original } // same as Object.assign: 얉은 복사💩
+  const result = _.cloneDeep(original)
+  result.baseCharge = calculateBaseCharge(result)
+  result.taxableCharge = Math.max(
+    0,
+    result.baseCharge - taxThreshold(result.year)
+  )
+  return result
+}
+
+function calculateBaseCharge(reading) {
+  return baseRate(reading.month, reading.year) * reading.quantity
 }
 
 export function baseRate(month, year) {
