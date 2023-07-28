220d400052ee58e11d593e8a8c307205b7a929b9
Tae-hyeon Kim <simoltobella@gmail.com>
2023-07-28 20:30:00 +0900

    Refactor: 6-11-2.js
    단계 쪼개기

--- ch6/6-11-2.js
+++ ch6/6-11-2.js
@@ -1,18 +1,31 @@
 import fs from 'fs'
 
-if (!process.argv[2]) {
-  throw new Error('파일 이름을 입력하세요')
+function run(args) {
+  const command = parseCommand(args)
+  countOrders(command)
 }
 
-const fileName = `./${process.argv[2]}.json`
-if (!fs.existsSync(fileName)) {
-  throw new Error('파일이 존재하지 않습니다')
+function parseCommand(args) {
+  if (!args[2]) throw new Error('파일 이름을 입력하세요')
+
+  const fileName = `./${args[2]}.json`
+  if (!fs.existsSync(fileName)) throw new Error('파일이 존재하지 않습니다')
+
+  const countReadyOnly = args.includes('-r')
+
+  return {
+    fileName,
+    countReadyOnly,
+  }
 }
 
-const rawData = fs.readFileSync(fileName)
-const orders = JSON.parse(rawData)
-if (process.argv.includes('-r')) {
-  console.log(orders.filter(order => order.status === 'ready').length)
-} else {
-  console.log(orders.length)
+function countOrders(command) {
+  const rawData = fs.readFileSync(command.fileName)
+  const orders = JSON.parse(rawData)
+  const filtered = command.countReadyOnly
+    ? orders.filter(order => order.status === 'ready').length
+    : orders.length
+  console.log(filtered)
 }
+
+run(process.argv)
