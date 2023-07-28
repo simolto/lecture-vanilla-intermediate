d78d7b9360c01a8be5a7155c2586b806f0c2655b
Tae-hyeon Kim <simoltobella@gmail.com>
2023-07-28 17:43:11 +0900

    Refactor: 6-8.js
    매개변수 객체 생성

--- ch6/6-8.js
+++ ch6/6-8.js
@@ -1,5 +1,22 @@
-export function readingsOutsideRange(station, min, max) {
-  return station.readings.filter(r => r.temp < min || r.temp > max)
+export function readingsOutsideRange(station, range) {
+  return station.readings.filter(r => r.temp < range.min || r.temp > range.max)
+}
+
+export class NumberRange {
+  #min
+  #max
+  constructor(min, max) {
+    this.#min = min
+    this.#max = max
+  }
+
+  get min() {
+    return this.#min
+  }
+
+  get max() {
+    return this.#max
+  }
 }
 
 const station = {
@@ -12,13 +29,6 @@ const station = {
     { temp: 51, time: '2016-11-10 09:50' },
   ],
 }
-const operationPlan = {
-  temperatureFloor: 51,
-  temperatureCeiling: 53,
-}
+const operationPlan = new NumberRange(51, 53)
 
-readingsOutsideRange(
-  station,
-  operationPlan.temperatureFloor,
-  operationPlan.temperatureCeiling
-)
+readingsOutsideRange(station, operationPlan)
