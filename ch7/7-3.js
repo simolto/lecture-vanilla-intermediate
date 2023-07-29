971a20f7c345e7c8d4a40a03a908522c97e1f28c
Tae-hyeon Kim <simoltobella@gmail.com>
2023-07-29 20:50:02 +0900

    Refactor: 7-3.js
    기본형을 객체로 변경

--- ch7/7-3.js
+++ ch7/7-3.js
@@ -2,6 +2,10 @@ export class Order {
   constructor(data) {
     this.priority = data.priority
   }
+
+  isHighPriority() {
+    return 'high' === this.priority || 'rush' === this.priority
+  }
 }
 
 const orders = [
@@ -10,6 +14,5 @@ const orders = [
   new Order({ priority: 'rush' }),
 ]
 
-const highPriorityCount = orders.filter(
-  o => 'high' === o.priority || 'rush' === o.priority
-).length
+const highPriorityCount = orders.filter(o => o.isHighPriority()).length
+console.log(highPriorityCount)
