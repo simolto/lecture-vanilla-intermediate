7c5187ab4a701c9156c09ffa1eca47d07d590b07
Tae-hyeon Kim <simoltobella@gmail.com>
2023-07-29 23:26:08 +0900

    Refactor: 7-9.js
    알고리즘 변경

--- ch7/7-9.js
+++ ch7/7-9.js
@@ -1,19 +1,12 @@
 function foundPerson(people) {
   for (let i = 0; i < people.length; i++) {
-    if (people[i] === 'Don') {
-      return 'Don'
-    }
-    if (people[i] === 'John') {
-      return 'John'
-    }
-    if (people[i] === 'Kent') {
-      return 'Kent'
-    }
+    const candidates = ['Don', 'John', 'Kent']
+    return people.find(p => candidates.includes(p)) || ''
   }
-  return ''
 }
 
 console.log(foundPerson(['John']))
 console.log(foundPerson(['Don', 'John']))
 console.log(foundPerson(['Kent', 'Don', 'John']))
+
 console.log(foundPerson(['Lisa', 'Don', 'Tom']))
