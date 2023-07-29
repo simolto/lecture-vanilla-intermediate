2cb0ddf07874b765da3646c05535b6f063b4df88
Tae-hyeon Kim <simoltobella@gmail.com>
2023-07-29 23:13:59 +0900

    Refactor: 7-6.js
    클래스 인라인

--- ch7/7-6.js
+++ ch7/7-6.js
@@ -51,3 +51,5 @@ console.log(shipment.trackingInfo)
 
 shipment.trackingInformation.shippingCompany = 'COSCO'
 console.log(shipment.trackingInfo)
+
+// 현재 프로젝트의 진행 여부에 따라서 클래스 추출 및 인라인에 대한 의사 결정을 자유롭게 진행하도록 할 것
