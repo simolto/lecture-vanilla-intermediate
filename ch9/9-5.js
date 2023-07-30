436615d6b67305d1a82d84a77176bfe994cc49d1
Tae-hyeon Kim <simoltobella@gmail.com>
2023-07-30 15:33:37 +0900

    Refactor: 9-5.js
    값을 참조로 변경

--- ch9/9-5.js
+++ ch9/9-5.js
@@ -1,20 +1,44 @@
-class Order {
-  constructor(data) {
-    this._number = data.number
-    this._customer = new Customer(data.customerId)
-  }
+const customerRepository = new CustomerRepository()
 
+const order = new Order(
+  data.number,
+  customerRepository.registerCustomer(data.customerId)
+)
+
+class Order {
+  constructor(number, customer) {
+    this._number = number
+    this._customer = customer
+  }
   get customer() {
     return this._customer
   }
 }
 
 class Customer {
-  constructor(id) {
+  constructor(id, name) {
     this._id = id
   }
 
   get id() {
     return this._id
+  }
+}
+
+class CustomerRepository {
+  #customers
+  constructor() {
+    this.#customers = new Map()
+  }
+
+  registerCustomer(id) {
+    if (!this.#customers.had(id)) {
+      this.#customers.set(id, new Customer(id))
+    }
+    return findCustomer(id)
+  }
+
+  findCustomer(id) {
+    return this.#customers.get(id)
   }
 }
