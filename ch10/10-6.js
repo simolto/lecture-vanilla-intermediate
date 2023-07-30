e39aa66b41718f1dd128c02605af697d5dc4a21f
Tae-hyeon Kim <simoltobella@gmail.com>
2023-07-30 17:26:48 +0900

    Refactor: 10-6.js
    어서션 추가

--- ch10/10-6.js
+++ ch10/10-6.js
@@ -1,8 +1,13 @@
+import { strict as assert } from 'node:assert'
+
 class Customer {
   constructor() {
     this.discountRate = 0
   }
   applyDiscount(number) {
+    assert(num >= 0)
     return this.discountRate ? number - this.discountRate * number : number
   }
 }
+
+new Customer.applyDiscount(1)
